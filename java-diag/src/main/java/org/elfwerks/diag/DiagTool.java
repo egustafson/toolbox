c3b885e3e6460e85b220fffbaad1dd5b6cc3cbfa
Eric Gustafson <eg-git@elfwerks.org>
2014-03-25 18:29:27 -0600

    added -d flag to keep diag running, added apache commons-cli

--- java-diag/src/main/java/org/elfwerks/diag/DiagTool.java
+++ java-diag/src/main/java/org/elfwerks/diag/DiagTool.java
@@ -1,9 +1,36 @@
 package org.elfwerks.diag;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class DiagTool {
 
-  public static void main(String[] args) {
-    
+  public static CommandLine parseCommandLine(String[] args) throws ParseException {
+    Options options = new Options();
+
+    options.addOption("d", "daemon", false, "daemonize");
+
+    CommandLineParser cmdLineParser = new BasicParser();
+    CommandLine cmdLine = cmdLineParser.parse(options, args);
+    return cmdLine;
+  }
+
+
+  public static void main(String[] args) throws Exception {
+    CommandLine cmdLine = parseCommandLine(args);
+
+    System.out.println("java.version: "+System.getProperties().getProperty("java.version"));
+
+    if ( cmdLine.hasOption('d') ) {
+        System.out.println("daemonizing, press ^C to exit...");
+        while ( true ) {
+            Thread.sleep(1000);
+        }
+    }
+
     System.out.println("[stdout] done.");
   }
 
